bc7af4af0b07ac8753d3ac2eed95a6e43aee66b6
OutOfMyFuckingMind <chadb369@gmail.com>
2017-11-12 19:23:14 +0400

    теперь точно допилил простенькую регистрацию. наверное, кек

--- test/registration.php
+++ test/registration.php
@@ -1,23 +1,39 @@
 <?php
 	$link = mysqli_connect ("test", "root", "", "test")
-		or die("Ошибка");
-        
-    $result = mysqli_query($link, "SELECT login FROM users WHERE login = '".$_POST['login']."'")
-        or die ('Не удалось подключиться к базе');
-        
-    if($result == null){
-        
-    $sql = mysqli_query($link, "INSERT INTO users (login, password)
-                                VALUES ('".htmlspecialchars($_POST['login'])."', '".htmlspecialchars($_POST['password'])."')");	
-            
-    mysqli_close($link);
-            
-    }
+		or die ("Ошибка");
 
-    else {
+	if($_POST['login'] != NULL) {
+        
+//		echo htmlspecialchars($_POST['login']);
+        
+//        $login = $_POST['login'];
+        
+        $result = mysqli_query($link, "SELECT login FROM users WHERE login = '".$_POST['login']."'")
+            or die ('Не удалось подключиться к базе');
+        
+        $result = mysqli_num_rows($result);
+        
+        if($result == null){
             
-        echo 'login занят';
+		$sql = mysqli_query($link, "INSERT INTO users (login, password)
+                                    VALUES ('".htmlspecialchars($_POST['login'])."', '".htmlspecialchars($_POST['password'])."')");	
+            
+		mysqli_close($link);
+            
+        header( 'Location: test/private-office.htm', true, 303 );
+            
+        }
+        else {
+            
+            echo 'login занят';
                 
-    }
+        }
+        
+	}
 
+	elseif($_POST['login'] == NULL) {
+        
+		echo 'Вы не ввели логин';
+        
+	}
 ?>
